a1e9fbee5788a2e1e298269bc358ca72f22283fe
Arnav Baliyan <baliyan.arnav@gmail.com>
2023-12-15 22:07:44 -0500

    fixed elint error

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
-import { DegreeRequirements } from "./DegreeReqs";
+//import { DegreeRequirements } from "./DegreeReqs";
 
 test("renders the course name somewhere", () => {
     render(<App />);
